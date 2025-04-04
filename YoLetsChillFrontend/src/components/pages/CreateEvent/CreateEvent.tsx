e5a47d3b66533b6601b53e70ba8fb120e2e0c530
David Beshensky <beshenskydave@gmail.com>
2025-04-04 18:50:25 -0500

    rebase compree

---
+++ YoLetsChillFrontend/src/components/pages/CreateEvent/CreateEvent.tsx
@@ -0,0 +1,14 @@
+// src/components/pages/CreateEvent.tsx
+import React from "react";
+
+const CreateEvent = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Create a New Event</h1>
+      <p>This page will let a host configure and start a new event.</p>
+      {/* Later: add form for event name, description, and time slots */}
+    </div>
+  );
+};
+
+export default CreateEvent;
